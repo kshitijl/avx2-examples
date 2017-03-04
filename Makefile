a5304d59d77086d29e66562172fc945bd3f5f081
Kshitij Lauria <kslauria@gmail.com>
2017-03-04 01:51:01 -0500

    Dot-product example

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 CC=g++ -std=c++11 -mavx2
 
+all: bin/01-hello bin/04-dot-product
+
 bin/01-hello: examples/01-hello.c
 	$(CC) examples/01-hello.c -o bin/01-hello
+
+bin/04-dot-product: examples/04-dot-product.c
+	$(CC) examples/04-dot-product.c -o bin/04-dot-product
---
+++ examples/04-dot-product.c
@@ -0,0 +1,54 @@
+/* Compute the dot product of two (properly aligned) vectors. */
+#include <stdio.h>
+#include <immintrin.h>
+#include <math.h>
+
+const int N = 80;
+
+double slow_dot_product(const double *a, const double *b) {
+  double answer = 0.0;
+  for(int ii = 0; ii < N; ++ii)
+    answer += a[ii]*b[ii];
+  return answer;
+}
+
+/* Horizontal add works within 128-bit lanes. Use scalar ops to add
+ * across the boundary. */
+double reduce_vector1(__m256d input) {
+  __m256d temp = _mm256_hadd_pd(input, input);
+  return ((double*)&temp)[0] + ((double*)&temp)[2];
+}
+
+/* Another way to get around the 128-bit boundary: grab the first 128
+ * bits, grab the lower 128 bits and then add them together with a 128
+ * bit add instruction. */
+double reduce_vector2(__m256d input) {
+  __m256d temp = _mm256_hadd_pd(input, input);
+  __m128d sum_high = _mm256_extractf128_pd(temp, 1);
+  __m128d result = _mm_add_pd(sum_high, _mm256_castpd256_pd128(temp));
+  return ((double*)&result)[0];
+}
+
+double dot_product(const double *a, const double *b) {
+  __m256d sum_vec = _mm256_set_pd(0.0, 0.0, 0.0, 0.0);
+  
+  for(int ii = 0; ii < N/4; ++ii) {
+    __m256d x = _mm256_load_pd(a+4*ii);
+    __m256d y = _mm256_load_pd(b+4*ii);
+    __m256d z = _mm256_mul_pd(x,y);
+    sum_vec = _mm256_add_pd(sum_vec, z);
+  }
+
+  return reduce_vector2(sum_vec);
+}
+
+int main() {
+  __attribute__ ((aligned (32))) double a[N], b[N];
+
+  for(int ii = 0; ii < N; ++ii)
+    a[ii] = b[ii] = ii/sqrt(N);
+
+  double answer = dot_product(a, b);
+  printf("%f\n", answer);
+  printf("%f\n", slow_dot_product(a,b));
+}
